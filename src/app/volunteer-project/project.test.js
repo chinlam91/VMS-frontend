7adf0733c63cb6c116e0df661d8f3c3f80515d2f
chinlam91 <chinlam91@hotmail.com>
2015-12-10 01:54:01 +0800

    unit test bug problem

---
+++ src/app/volunteer-project/project.test.js
@@ -0,0 +1,67 @@
+// (function() {
+//     'use strict';
+
+//     describe('projects test', function() {
+//         beforeEach(module('vmsFrontend'));
+
+//         var $controller;
+
+//         beforeEach(inject(function(_$controller_) {
+//             $controller = _$controller_;
+//         }));
+
+//         describe('variable declare', function() {
+//             it('test', function() {
+//                 var $scope = {};
+//                 var controller = $controller('projectController', {
+//                     $scope: $scope
+//                 });
+// expect($scope.projects).toBeDefined();
+// expect($scope.volunteers).toBeDefined();
+// expect($scope.manager).toBeDefined();
+// expect($scope.managerMail).toBeDefined();
+// expect($scope.createdDate).toBeDefined();
+// expect($scope.description).toBeDefined();
+// expect($scope.link).toBeDefined();
+// expect($scope.position).toBeDefined();
+// expect($scope.students).toBeDefined();
+// expect($scope.school).toBeDefined();
+// expect($scope.participated).toBeDefined();
+
+//             });
+//         });
+//     });
+// })();
+
+
+describe('projects testing', function() {
+
+    beforeEach(module('vmsFrontend'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
+
+    describe('variable', function() {
+        it('declare', function() {
+            var $scope = {};
+            var controller = $controller('projectController', {
+                $scope: $scope
+            });
+            expect($scope.projects).toBeDefined();
+            expect($scope.volunteers).toBeDefined();
+            expect($scope.manager).toBeDefined();
+            expect($scope.managerMail).toBeDefined();
+            expect($scope.createdDate).toBeDefined();
+            expect($scope.description).toBeDefined();
+            expect($scope.link).toBeDefined();
+            expect($scope.position).toBeDefined();
+            expect($scope.students).toBeDefined();
+            expect($scope.school).toBeDefined();
+            expect($scope.participated).toBeDefined();
+        });
+    });
+
+});
